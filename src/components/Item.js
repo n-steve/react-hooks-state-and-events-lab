9ce4f7a15f2fa40dee3218969717535081f7992c
steve <steve.nguyen2013@gmail.com>
2022-05-29 22:50:15 -0700

    not done yet

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isOn, setIsOn] = useState(false);
+  function toggleCart() {
+    setIsOn((isOn) => !isOn);
+  }
   return (
-    <li className="">
+    <li className={isOn ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {isOn ? (
+        <button className="add" onClick={toggleCart}>
+          Add To Cart
+        </button>
+      ) : (
+        <button className="remove" onClick={toggleCart}>
+          Remove From Cart
+        </button>
+      )}
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  console.log(items);
+  // const [selectedCategory,setFilterBy] = useState("filter");
+  // const foodsToDisplay =
   return (
     <div className="ShoppingList">
       <div className="Filter">
