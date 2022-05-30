e03a1fb0be3a2dbec7ead02f36d18a84a85ee338
steve <steve.nguyen2013@gmail.com>
2022-05-30 14:19:39 -0700

    completed assignmenet

--- src/components/App.js
+++ src/components/App.js
@@ -8,20 +8,15 @@ function App() {
   function toggleModes() {
     setIsOn((isOn) => !isOn);
   }
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-
+  const color = isOn ? "dark" : "light";
   return (
     <div className={isOn ? "App dark" : "App light"}>
       <header>
         <h2>Shopster</h2>
-        {isOn ? (
-          <button onClick={toggleModes}>Dark Mode</button>
-        ) : (
-          <button onDoubleClick={toggleModes}>Light Mode</button>
-        )}
+      <button style={{background:color}} onClick={toggleModes}>{isOn ? "Dark Mode" : "Light Mode"}</button> 
+
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={itemData} />d
     </div>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,13 +2,20 @@ import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
-  console.log(items);
-  // const [selectedCategory,setFilterBy] = useState("filter");
-  // const foodsToDisplay =
+  const [selectedCatergory, setSelectedCategory] = useState("All")
+
+  function handleCategory(event){
+    setSelectedCategory(event.target.value);
+  }
+
+  const itemInShop = items.filter((item)=>{
+    if( selectedCatergory === "All") return true;
+    return item.category === selectedCatergory;
+  })
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -16,7 +23,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemInShop.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
